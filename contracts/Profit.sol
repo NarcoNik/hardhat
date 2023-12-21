d9978fe6564c0ffb7a5b83f31f344cfc84abe0a9
NarcoNik <plakidin.vyacheslav@mail.ru>
2023-12-21 12:02:03 +0700

    «new»

--- contracts/Profit.sol
+++ contracts/Profit.sol
@@ -6,49 +6,76 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../libs/SafeMathInt.sol";
 import "../libs/SafeMathUint.sol";
 
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../libs/SafeMathInt.sol";
+import "../libs/SafeMathUint.sol";
+
 contract DynamicWeightedLP {
     using Address for address payable;
     using SafeMath for uint256;
     using SafeMathUint for uint256;
     using SafeMathInt for int256;
 
-    uint256 public allLP;
-    uint256 public allLPt;
-
     struct UserInfo {
-        uint256 t;
+        uint256 lastUpdateTime;
         uint256 LP;
         uint256 LPt;
     }
 
     mapping(address => UserInfo) userInfo;
 
+    uint256 private totalLPs;
+    uint256 private totalLPts;
+
     function deposit(uint256 LP) external {
-        uint256 t = block.timestamp;
+        UserInfo storage users = userInfo[msg.sender];
+        uint256 memory t = block.timestamp;
         uint256 LPt = LP * t;
-        userInfo[msg.sender] = UserInfo(t, LP, LPt); // Initialize weight to 0
-        allLP += LP;
-        allLPt += LPt;
+        if (users.LP <= 0) {
+            userInfo[msg.sender] = UserInfo(t, LP, LPt); // Initialize weight to 0
+        }
+        users.LP += LP;
+        users.LPt += LPt;
+        users.lastUpdateTime = t;
+        totalLPs += LP;
+        totalLPts += LPt;
     }
 
     function withdraw(uint256 LP) external {
-        UserInfo storage user = userInfo[msg.sender];
-        require(user.LP >= LP, "Insufficient LP amount");
+        UserInfo storage users = userInfo[msg.sender];
+        require(users.LP >= LP, "Insufficient LP amount");
 
-        uint256 t = block.timestamp;
-        user.LP -= LP;
-        uint256 newUserLPt = user.LP.mul(user.t);
+        uint256 memory t = block.timestamp;
+        users.LP -= LP;
+        uint256 oldUserLPt = users.LP.mul(t);
+        users.LPt -= LP;
 
-        user.LPt = newUserLPt.mul(t);
-        if (LP >= user.LP) {
+        users.LPt = (oldUserLPt + LP.mul(t)).div(2);
+        users.lastUpdateTime = t;
+
+        if (LP >= users.LP) {
             delete userInfo[msg.sender];
         }
     }
 
     function getPrivateLP(address user) external view returns (uint256) {
         UserInfo storage users = userInfo[user];
-        uint256 privateLP = (users.LPt * allLP) / allLPt;
+        uint256 privateLP = (users.LPt * totalLPs) / totalLPts;
         return privateLP;
+    }
+
+    function getPrivateNumber(address user) external view returns (uint256) {
+        // Calculate the user's weight in the pool based on the time intervals
+        uint256 elapsedTime = block.timestamp.sub(users[user].lastUpdateTime);
+        uint256 userWeight = users[user].lpBalance.mul(elapsedTime);
+
+        // Calculate the private number of LPs relative to the total number of LPs
+        uint256 privateNumber = userWeight.mul(totalLPs).div(totalLPs);
+
+        return privateNumber;
     }
 
     function getUserInfo(address user) external view returns (UserInfo memory) {
